5758ba8b2d8c16db008f0b58081b5ab914a236e8
MLesky <mbahlesky2@gmail.com>
2022-12-24 12:21:22 +0100

    Navigate through images

--- My app/myapp/lib/main.dart
+++ My app/myapp/lib/main.dart
@@ -1,31 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-  home: Home(),
-));
+List<String> images = ['smart-tech', 'man', 'flat-design'];
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  int index = 0;
+  String navigateText = 'next';
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My First Ninja App',),
+        title: Text('My First Ninja App $index',),
         centerTitle: true,
         backgroundColor: Colors.red[900],
       ),
-      body: const Center(
+      body: Center(
         child: Image(
-          image: AssetImage('assets/images/smart-tech.png'),
+          image: AssetImage('assets/images/${images[index]}.png'),
           //image: NetworkImage('https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?size=626&ext=jpg'),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
+        onPressed: navigateTo,
         backgroundColor: Colors.red[900],
-        child: const Text('click'),
+        child: Text(navigateText),
       ),
     );
+  }
+
+  void navigateTo() {
+    if(index >= images.length-1){
+      setState(() => index = 0);
+    } else {
+      setState(() => index++);
+    }
   }
 }
