d154a8593c9397d5c1d30a44eec20e51a247c995
MLesky <mbahlesky2@gmail.com>
2022-12-24 11:12:48 +0100

    refactor: StatelessWidget vs StatefulWidget override statement

--- My app/myapp/lib/main.dart
+++ My app/myapp/lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: const Text('My First Ninja App',),
-      centerTitle: true,
-      backgroundColor: Colors.red[900],
-    ),
-    body: Center(
+void main() => runApp(const MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My First Ninja App',),
+        centerTitle: true,
+        backgroundColor: Colors.red[900],
+      ),
+      body: Center(
         child: Text(
-            'Hello Lespa',
-            style: TextStyle(
+          'Hello Lespa',
+          style: TextStyle(
               fontSize: 20.0,
               fontWeight: FontWeight.bold,
               letterSpacing: 2.0,
@@ -18,11 +26,12 @@ void main() => runApp(MaterialApp(
               fontFamily: 'ScaryDance'
           ),
         ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () => {},
-      backgroundColor: Colors.red[900],
-      child: const Text('click'),
-    ),
-  ),
-));
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        backgroundColor: Colors.red[900],
+        child: const Text('click'),
+      ),
+    );
+  }
+}
