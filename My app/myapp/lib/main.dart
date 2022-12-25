e68c5461038118f96e5e20ed5d3480699967a388
MLesky <mbahlesky2@gmail.com>
2022-12-25 22:03:48 +0100

    map function (list)

--- My app/myapp/lib/main.dart
+++ My app/myapp/lib/main.dart
@@ -12,65 +12,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int minutes = 0, seconds = 0, hours = 0;
+  List<String> quotes = [
+    'Be yourself; everyone else is already taken',
+    'I have nothing to declare except my genius',
+    'The truth is rarely pure and never simple',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: const Text('My First Ninja App',),
+        title: const Text('Awesome Quotes'),
         centerTitle: true,
-        backgroundColor: Colors.red[900],
+        backgroundColor: Colors.redAccent,
       ),
-      body: Row(
-        children: <Widget>[
-          Expanded(
-              flex: 3,
-              child: Image.asset('assets/images/man.png'),
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: const EdgeInsets.all(30.0),
-              color: Colors.cyan,
-              child: Text('$seconds'),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              padding: const EdgeInsets.all(30.0),
-              color: Colors.pinkAccent,
-              child: Text('$minutes'),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: const EdgeInsets.all(30.0),
-              color: Colors.amber,
-              child: Text('$hours'),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() => seconds++);
-          if(seconds == 10){
-            setState((){
-              seconds = 0;
-              minutes++;
-            });
-          }
-          if(minutes == 10){
-            setState((){
-              minutes = 0;
-              hours++;
-            });
-          }
-        },
-        backgroundColor: Colors.red[900],
-        child: const Text('click'),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList(),
       ),
     );
   }
