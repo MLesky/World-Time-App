a03f740f719d052daa9c7e8781bdd7c851e2e5c4
MLesky <mbahlesky2@gmail.com>
2022-12-25 22:26:51 +0100

    custom class

--- My app/myapp/lib/main.dart
+++ My app/myapp/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'quote.dart';
+
 void main() => runApp(const MaterialApp(
   home: Home(),
 ));
@@ -12,10 +14,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<String> quotes = [
-    'Be yourself; everyone else is already taken',
-    'I have nothing to declare except my genius',
-    'The truth is rarely pure and never simple',
+  List<Quote> quotes = [
+    Quote(text: 'Be yourself; everyone else is already taken', author: 'Osca Wilde'),
+    Quote(text: 'I have nothing to declare except my genius', author: 'Osca Wilde'),
+    Quote(text: 'The truth is rarely pure and never simple', author: 'Osca Wilde'),
   ];
   @override
   Widget build(BuildContext context) {
@@ -27,7 +29,7 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text(quote)).toList(),
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
       ),
     );
   }
