0cbd3fee91b1bb9c6bd330cffa894cabd77faa58
MLesky <mbahlesky2@gmail.com>
2022-12-26 08:19:52 +0100

    Cards custom widget functions

--- My app/myapp/lib/main.dart
+++ My app/myapp/lib/main.dart
@@ -29,7 +29,40 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+      ),
+    );
+  }
+
+  Widget quoteTemplate(quote){
+    return Card(
+      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[800],
+              ),
+            ),
+            Divider(
+              height: 20.0,
+              thickness: 2.0,
+              color: Colors.grey[500],
+            ),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[600],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
