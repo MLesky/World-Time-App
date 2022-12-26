262d6f39b900f76e9465d08a7600e6b41a2fb5e1
MLesky <mbahlesky2@gmail.com>
2022-12-26 16:38:37 +0100

    qoutes: add and delete quotes

--- My app/myapp/lib/main.dart
+++ My app/myapp/lib/main.dart
@@ -20,6 +20,15 @@ class _HomeState extends State<Home> {
     Quote(text: 'I have nothing to declare except my genius', author: 'Osca Wilde'),
     Quote(text: 'The truth is rarely pure and never simple', author: 'Osca Wilde'),
   ];
+  String errorMessage = '';
+  late TextEditingController quoteInputField;
+
+  @override
+  void initState() {
+    super.initState();
+    quoteInputField = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,17 +38,79 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
-      body: Column(
-        children: quotes.map((quote) => QuoteCard(
-            quote: quote,
-            delete: () {
-              setState(() {
-                quotes.remove(quote);
-              });
-              print("\"${quote.text}\" by ${quote.author} has been deleted");
-            }
-        )).toList(),
+      body: ListView(
+        children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
+              child: Column(
+              children: [
+                  TextField(
+                    controller: quoteInputField,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'quote',
+                    ),
+                    onChanged: (e){
+                      if(quoteInputField.text.isEmpty){
+                        setState((){
+                          errorMessage = "Quote cannot be empty";
+                        });
+                      } else {
+                        setState((){
+                          errorMessage = "";
+                        });
+                      }
+                    },
+                  ),
+                if(errorMessage.isNotEmpty)
+                Text(
+                  errorMessage,
+                  style: TextStyle(
+                    color: Colors.red[800],
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+                const SizedBox(height: 5.0,),
+                ElevatedButton.icon(
+                    onPressed: (){
+                      String newQuoteText = quoteInputField.text;
+                      if(newQuoteText.isNotEmpty) {
+                        Quote newQuote = Quote(text: newQuoteText, author: 'Mbah Lesky');
+                        setState(() {
+                          quotes.add(newQuote);
+                        });
+                        print("\"$newQuoteText\" by Mbah Lesky has been added");
+                      }
+                    },
+                    icon: const Icon(Icons.add),
+                    label: const Text('add'),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.redAccent),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              children: quotes.map((quote) => QuoteCard(
+                  quote: quote,
+                  delete: () {
+                    setState(() {
+                      quotes.remove(quote);
+                    });
+                    print("\"${quote.text}\" by ${quote.author} has been deleted");
+                  }
+              )).toList(),
+            ),
+        ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    quoteInputField.dispose();
+    super.dispose();
   }
 }
