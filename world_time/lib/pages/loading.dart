a703f5d881e97697cdf989a8c3ba6abb7d330ff1
MLesky <mbahlesky2@gmail.com>
2022-12-30 14:10:58 +0100

    custom class

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -11,36 +10,30 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async {
+  String time = 'loading...';
 
-    // make the request
-    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Africa/Cairo'));
-    Map data = jsonDecode(response.body);
-    // print("data: $data");
-
-    // get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
-    // print("$datetime \n$offset");
-
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    // print(now);
-    now = now.add(Duration(hours: int.parse(offset)));
-    // print("now: $now");
-
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Cairo', flag: 'cameroon.png', url: 'Africa/Cairo');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Choose location screen'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
