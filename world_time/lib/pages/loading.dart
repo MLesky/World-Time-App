f61595cbe67e8f6f0b1fc2627734ecde4e01623e
MLesky <mbahlesky2@gmail.com>
2022-12-30 13:46:01 +0100

    the worldTime API, getting time for a location

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -11,17 +11,30 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
+  void getTime() async {
 
-   Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
-   Map data = jsonDecode(response.body);
-   print(data);
+    // make the request
+    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Africa/Cairo'));
+    Map data = jsonDecode(response.body);
+    // print("data: $data");
+
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    // print("$datetime \n$offset");
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    // print(now);
+    now = now.add(Duration(hours: int.parse(offset)));
+    // print("now: $now");
+
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
